30ae0ebdad24d925387025878d5481040c382665
concept360ch <44680517+concept360ch@users.noreply.github.com>
2018-11-06 16:19:16 +0100

    Fix project tiles

--- notio-wp-child/highlights.php
+++ notio-wp-child/highlights.php
@@ -2,21 +2,29 @@
 
 <?php get_header(); ?>
 
-<?php while ( have_posts() ) : the_post(); ?>
   <div class="highlights projects projects__highlights row">
     <?php $catquery = new WP_Query( 'type=post&cat=36&posts_per_page=-1' ); ?>
     <div class="projects__project-container">
-      <?php while($catquery->have_posts()) : $catquery->the_post(); ?>
-        <?php 
-          $image_id = get_post_meta($post->ID, 'project_image-main', true);
-          $image_url = wp_get_attachment_image_src( $image_id , 'large' )[0];
-          $post_description = get_post_meta($post->ID, 'project_description', true);
-          $post_description_length = strlen($post_description);
-          $post_description_short = get_post_meta($post->ID, 'project_teaser', true); 
-          $post_description_long = $post_description_length > 160 ? substr($post_description, 0, 160) . "..." : $post_description; 
+      <?php 
+	  $posts = $catquery->get_posts();
+	  $posts = c360_rearrange_posts($posts);
+	  
+	  // Set wrapper flag
+	  $wrapperFlag = 0;
+	  
+	  foreach($posts as $post) { //while($catquery->have_posts()) : $catquery->the_post(); 
+	
+		// Check for wrapper, as comment_count
+		if($post->comment_count == 1 && !$wrapperFlag) { $wrapperFlag = 1 ?><div class="project-wrapper" style="flex-basis: calc(100%/3); float: left"> <?php ; }
+		else if($post->comment_count == 0 && $wrapperFlag) { $wrapperFlag = 0; ?></div><?php }
+	
+		$post_description = get_post_meta($post->ID, 'project_description', true);
+		$post_description_length = strlen($post_description);
+		$post_description_short = get_post_meta($post->ID, 'project_teaser', true); 
+		$post_description_long = $post_description_length > 160 ? substr($post_description, 0, 160) . "..." : $post_description; 
         ?>
         <?php if(has_category(37)): ?>
-          <div class="projects__project project__insight project__thumbnail <?php echo get_post_meta($post->ID, 'highlight', true) ? 'projects__project__highlight' : '' ?>">
+          <div class="projects__project project__insight project__thumbnail <?php echo get_post_meta($post->ID, 'highlight', true) && $post->comment_status != 1 ? 'projects__project__highlight' : '' ?>">
             <a href="<?php the_permalink() ?>">
               <div>
                 <h3><?php the_title(); ?></h3>
@@ -24,10 +32,10 @@
             </a>
           </div>
         <?php else: ?>
-          <div class="projects__project project__thumbnail <?php echo get_post_meta($post->ID, 'highlight', true) ? 'projects__project__highlight' : '' ?>">
+          <div class="projects__project project__thumbnail <?php echo get_post_meta($post->ID, 'highlight', true) && $post->comment_status != 1 ? 'projects__project__highlight' : '' ?>">
             <a href="<?php the_permalink() ?>">
               <div>
-                <?php echo the_post_thumbnail($post->ID) ? the_post_thumbnail($post->ID) : '<p>Bitte Bild hinzufügen</p>'; ?>
+                <?php echo the_post_thumbnail($post->ID) ? the_post_thumbnail($post->ID) : ''; ?>
                 <div class="projects__project__info project-hover-info">
                   <h3><?php echo the_title(); ?></h3>
                   <p><?php echo $post_description_short ?></p>
@@ -37,17 +45,11 @@
             </a>
           </div>
         <?php endif; ?>
-      <?php endwhile;
+      <?php } 
         wp_reset_postdata();
       ?>
     </div>
   </div>
-<?php endwhile; ?>
-
-
 <?php
-
-
   get_footer();
-
 ?>
